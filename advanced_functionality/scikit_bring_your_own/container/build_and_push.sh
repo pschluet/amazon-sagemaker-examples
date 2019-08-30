fe7bb54d5d15669502975f77dbef90ea846b5283
Paul Schlueter <paul@paulschlueter.com>
2019-08-30 15:34:00 -0500

    Made Notebook Based on Production Docker Image

--- advanced_functionality/scikit_bring_your_own/container/build_and_push.sh
+++ advanced_functionality/scikit_bring_your_own/container/build_and_push.sh
@@ -3,15 +3,9 @@
 # This script shows how to build the Docker image and push it to ECR to be ready for use
 # by SageMaker.
 
-# The argument to this script is the image name. This will be used as the image on the local
-# machine and combined with the account and region to form the repository name for ECR.
-image=$1
-
-if [ "$image" == "" ]
-then
-    echo "Usage: $0 <image-name>"
-    exit 1
-fi
+# This will be used as the image on the local machine and combined with the account and 
+# region to form the repository name for ECR.
+image=scikit
 
 chmod +x algorithm/train
 chmod +x algorithm/serve
